99f26e72f16c605677aa074ac8beb11d890ece43
victorjp1 <victorjp999@gmail.com>
2020-02-20 19:03:35 +0100

    Mejora ejer2

--- src/main/java/com/victorlopez/Ejercicio02/Main.java
+++ src/main/java/com/victorlopez/Ejercicio02/Main.java
@@ -2,18 +2,20 @@ package com.victorlopez.Ejercicio02;
 
 import com.victorlopez.Lib;
 
-import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) {
         ArrayListEstadisticas estadisticas = new ArrayListEstadisticas();//Creamos el nuevo objeto array list
         Lib.llenarArrayListAleatorio(estadisticas, 10);//Lo llenamos de forma aleatoria con 10 elementos
-        System.out.println("El Array List es: " + estadisticas.toString());
-        System.out.printf("La media del Array es: %2.2f %n", estadisticas.media());
-        System.out.printf("El mínimo es: %2.2f %n", estadisticas.minimo());
-        System.out.printf("El máximo es: %2.2f %n", estadisticas.maximo());
-        System.out.printf("El sumatorio es: %2.2f %n", estadisticas.sumatorio());
-        System.out.printf("El valor más repetido es: %2.2f %n", estadisticas.moda());
-        System.out.printf("El valor más repetido es: %2.2f %n", estadisticas.modaHashMap());
+        imprimirResumen(estadisticas);
+
+    }
+    public static void imprimirResumen(ArrayListEstadisticas a){
+        System.out.println("El Array List es: " + a.toString());
+        System.out.printf("La media del Array es: %2.2f %n", a.media());
+        System.out.printf("El mínimo es: %2.2f %n", a.minimo());
+        System.out.printf("El máximo es: %2.2f %n", a.maximo());
+        System.out.printf("El sumatorio es: %2.2f %n", a.sumatorio());
+        System.out.printf("El valor más repetido es: %2.2f %n", a.moda());
+        System.out.printf("El valor más repetido encontrado con HashMap es: %2.2f %n", a.modaHashMap());
     }
 }
